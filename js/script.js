cacac9db5278bf38c99a4763e1e127c8fc20fe12
Pankaj Dadwal <pt8699469948@gmail.com>
2020-07-25 02:32:48 +0530

    Landing page done

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+    // $('button').click((e)=>{
+    //     e.preventDefault();
+    //     let user = 'pankaj';
+    //     let pass = "1234";
+        
+
+    //     let uname = $(".uname").val();
+
+    //     let upass = $(".upass").val();
+
+    //     if(user == uname && pass == upass){
+
+    //         $('h1').text("Welcome "+uname );
+
+
+
+    //     }
+    //     else{
+    //         $('h1').text("Wrong BHG " );
+    //     }
+        
+
+
+    // })
+
+
+$('.nav--trigger').click(()=>{
+
+    $('.nav__bar').toggleClass("display-block")
+
+})
